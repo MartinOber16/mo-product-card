6bb4004dab88a9c8b1f527302bdcd1400d980746
Martin Obermeier <martin.obermeier@gmail.com>
2021-12-23 21:10:02 -0300

    Version 0.0.1 lista

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ProductImage, ProductCard } from '../../src/components';
+import { product2 } from '../data/products';
+
+describe('ProductImage', () => {
+
+  test('debe mostrar el componente correctamente con la imagen personalizada', () => {
+
+    const wrapper = renderer.create(
+        <ProductImage img="http://imagen.jpg" />
+    )
+
+    expect(wrapper.toJSON()).toMatchSnapshot();
+
+  });
+
+  test('debe mostrar el componente con la imagen del producto', () => {
+    const wrapper = renderer.create(
+        <ProductCard product={ product2 } >
+            {
+                () => (
+                    <ProductImage />
+                )
+            }
+        </ProductCard>
+    )
+
+    expect(wrapper.toJSON()).toMatchSnapshot();
+
+  });
+  
+});
